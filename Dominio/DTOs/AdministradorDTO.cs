57eba03dbfbe014eb9008f82006ff1753a3d556f
João Vítor F Martiniano <115907286+JoaoV-Mart@users.noreply.github.com>
2024-10-21 09:07:06 -0300

    Update AdministradorDTO.cs

--- Dominio/DTOs/AdministradorDTO.cs
+++ Dominio/DTOs/AdministradorDTO.cs
@@ -1,1 +1,10 @@
+namespace MinimalsApi.DTOs;
 
+public class AdministradorDTO
+{
+  public string Email{ get; set; } = default!;
+
+  public string Senha{ get; set; } = default!;
+
+  public string Perfil{ get; set; } = default!;
+}
